9af4a1b46e83e6550263f44ccf9ac3ee878436a2
Wei-chen LIN <weilin@student.42.fr>
2019-01-05 19:25:17 +0100

    single fd filechecker passed

--- get_next_line.c
+++ get_next_line.c
@@ -6,50 +6,55 @@
 /*   By: weilin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 19:27:10 by weilin            #+#    #+#             */
-/*   Updated: 2019/01/05 12:51:54 by weilin           ###   ########.fr       */
+/*   Updated: 2019/01/05 19:22:40 by weilin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-#define MAX_FD 1024
-
-int get_next_line(const int fd, char **line, int nol)
+int get_next_line(const int fd, char **line)
 {
 	static char *rest = NULL;
 	int rd;
-	char buff[BUFF_SIZE + 1];
-	char *tmp;
+	char *buff;
 	char *n;
 
-	if (!fd || !line || read(fd, NULL, 0) < 0)
+	if (fd < 0 || !line || read(fd, NULL, 0) < 0 || BUFF_SIZE < 1 || 
+		!(buff = ft_strnew(BUFF_SIZE)))
 		return (-1);
 	while ((rd = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		buff[BUFF_SIZE] = '\0';
 		if (rest)
-			rest = ft_strjoin(rest, buff);
+			rest = ft_cleanswap(rest, ft_strjoin(rest, buff));
 		else
 			rest = ft_strdup(buff);
 		if (ft_strchr(buff, '\n'))
 			break;
+		ft_strclr(buff);
 	}
+	ft_strdel(&buff);
 	if (rest != '\0' && (n = ft_strchr(rest, '\n')))
 	{
-		*line = ft_strsub(rest, 0, n - rest);
-		rest = ft_strsub(rest, n - rest + 1, ft_strlen(rest) - (n - rest) - 1);
+		int cpylen = n - rest;
+		int restlen = ft_strlen(rest);
+		*line = ft_strsub(rest, 0, cpylen);
+		rest = ft_cleanswap(rest,
+				ft_strsub(rest, cpylen + 1, restlen - cpylen - 1));
 		return (1);
 	}
 	else if (rest != '\0')
 	{
 		*line = ft_strdup(rest);
-		rest = NULL;
+		ft_strdel(&rest);
+		if(ft_strlen(*line) == 0 && rd == 0)
+			return(0);
 		return (1);
 	}
-	//	if (rd == 0 && rest == NULL)
+	ft_strdel(&rest);
 	return (0);
 }
+
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
@@ -66,15 +71,13 @@ int main(int argc, char **argv)
 	//char *line2;
 	//nol2= 0;
 	fd = open(argv[1], O_RDONLY);
-	while ((ret = get_next_line(fd, &line, nol)) > 0)
+	while ((ret = get_next_line(fd, &line)) > 0)
 	{
 		printf("[Return: %d] Line #%.2d: %s\n", ret, ++nol, line);
-		free(line);
+		ft_strdel(&line);
 	}
 	printf("[Return: %d] Line #%.2d: %s = Last Line\n", ret, nol, line);
-	get_next_line(fd, &line, nol);
-	printf("[Return: %d] Line #%.2d: %s = Last Line\n", ret, nol, line);
-	get_next_line(fd, &line, nol);
+	get_next_line(fd, &line);
 	printf("[Return: %d] Line #%.2d: %s = Last Line\n", ret, nol, line);
 	if (ret == -1)
 		printf("-----------Error\n");
@@ -82,53 +85,16 @@ int main(int argc, char **argv)
 		printf("-----------End of file\n");
 	close(fd);
 	/*fd2 = open(argv[2], O_RDONLY);
-		while ((ret2 = get_next_line(fd2, &line2)) > 0)
-		{
-			printf("[Return: %d] Line #%d: %s\n", ret2, ++nol2, line2);
-			free(line2);
-		}
-		if (ret2 == -1)
-			printf("-----------Error\n");
-		else if (ret2 == 0)
-			printf("-----------End of file\n");
-		close(fd2);*/
+	while ((ret2 = get_next_line(fd2, &line2)) > 0)
+	{
+		printf("[Return: %d] Line #%d: %s\n", ret2, ++nol2, line2);
+		free(line2);
+	}
+	if (ret2 == -1)
+		printf("-----------Error\n");
+	else if (ret2 == 0)
+		printf("-----------End of file\n");
+	close(fd2);*/
 	return (0);
 }
 
-/*
-txt1:
-Hello\n
-World\n
- new line
-last line
-djfbkjdhf
-
-buff=8
-[Hello\nWo]
-
-Wo+rld
-
-buff=[rld\nnew]
-
-
-[Hello]
-fd,[Wo]
-
-[Wo] + [rld\n new]
-[World]
-fd, [ new]
-
-txt2:
-Other file
-only 2 lines
-
-fd=1 txt==4 lines
-|table = {"World\n new line\nlast line"}
--> line = "Hello"
-
-static char** fd[409600]
-bzero(fd, sizeof(abc));
-
-char **fd[0~409600]
-java
-*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,17 @@
 /*   By: weilin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/23 19:29:30 by weilin            #+#    #+#             */
-/*   Updated: 2018/12/07 19:40:35 by weilin           ###   ########.fr       */
+/*   Updated: 2019/01/05 17:10:46 by weilin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
-# define BUFF_SIZE 5
+#define GET_NEXT_LINE_H
+#define BUFF_SIZE 2
 
-# include "libft/libft.h"
-# include <fcntl.h>
+#include "libft/libft.h"
+#include <fcntl.h>
 
-int	get_next_line(const int fd, char **line);
+int get_next_line(const int fd, char **line);
 
 #endif
